80eafe20cbd3d6dab6c63d384a635a42cb1c8cc1
Wonsterr <viniciusrodriguessouzalima@gmail.com>
2026-08-28 22:33:01 -0300

    add pre-loader em todas pag e ajeitei o botao de login e cadastro

---
+++ assets/js/preLoader.js
@@ -0,0 +1,9 @@
+// Espera a página terminar de carregar
+window.addEventListener("load", function () {
+
+    const preloader = document.getElementById("preloader");
+
+    // Esconde a tela de carregamento
+    preloader.classList.add("preloader-escondido");
+
+});
