be698de0995bd2398b979303f34afaffff5d5892
moreiirax <miguelaugusto082006@gmail.com>
2026-08-30 01:23:56 -0300

    efeito ed agua

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,18 @@
 import LiquidBackground from 'https://cdn.jsdelivr.net/npm/threejs-components@0.0.27/build/backgrounds/liquid1.min.js';
 
-const canvas = document.getElementById('liquid-canvas');
+const canvases = document.querySelectorAll('.liquid-canvas');
 
-const app = LiquidBackground(canvas);
+canvases.forEach((canvas) => {
 
-app.loadImage('./assets/img/img2-fundo-site.png');
+    const app = LiquidBackground(canvas);
 
-app.liquidPlane.material.metalness = 0.45;
-app.liquidPlane.material.roughness = 0.3;
+    app.loadImage('./assets/img/img2-fundo-site.png');
 
-app.liquidPlane.uniforms.displacementScale.value = 2.5;
+    app.liquidPlane.material.metalness = 0.45;
+    app.liquidPlane.material.roughness = 0.3;
 
-app.setRain(false);
+    app.liquidPlane.uniforms.displacementScale.value = 2.5;
+
+    app.setRain(false);
+
+});
